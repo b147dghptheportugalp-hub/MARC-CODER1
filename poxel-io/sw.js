114e200a3b2eca7e50eb83e81f5125216404ab07
Emmanuel Umuakpero <emmanuelumuakpero@gmail.com>
2026-09-21 17:24:28 +0100

    Add pistol FBX model

--- poxel-io/sw.js
+++ poxel-io/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poxel-io-v2';
+const CACHE_NAME = 'poxel-io-v4';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {

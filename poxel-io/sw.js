3069590182c30d9c4110a6d7c7d15cf5055b9853
Emmanuel Umuakpero <emmanuelumuakpero@gmail.com>
2026-09-21 17:49:02 +0100

    Assign models to weapon families

--- poxel-io/sw.js
+++ poxel-io/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poxel-io-v5';
+const CACHE_NAME = 'poxel-io-v6';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
